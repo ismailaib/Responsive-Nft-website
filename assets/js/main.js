c863d626408eb90467902396eea055ec74a8f24c
ismailaib <ismailaitbouhmad@gmail.com>
2022-10-15 15:47:26 +0100

    add animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,16 @@ window.addEventListener("mousemove", e => {
   yTo(e.pageY);
 });
 
+const rocketSelector = document.querySelector("[data-rocket]");
+const rocketImageSelector = document.querySelector("[data-rocket-image]");
+
+rocketSelector.addEventListener("mousemove", function (event) {
+  const x = event.clientX - window.innerWidth / 2;
+  const y = event.clientY - window.innerHeight / 2;
+
+  rocketImageSelector.style.transform = `translate3d(${x / 10}px, ${y / 10}px, 0)`;
+});
+
+rocketSelector.addEventListener("mouseleave", function () {
+  rocketImageSelector.style.transform = "translate3d(0, 0, 0)";
+});
